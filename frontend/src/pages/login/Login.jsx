2a62bc9025b3a57eb29b989c5583ec2ec7d5be1a
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-24 10:49:42 -0500

    completed login func

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useLogin from '../../hooks/useLogin'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { loading, login } = useLogin()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await login(username, password)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -11,7 +23,7 @@ const Login = () => {
           </span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className='label p-2'>
               <span className='text-base text-slate-300 label-text'>
@@ -22,6 +34,8 @@ const Login = () => {
               type='text'
               placeholder='Enter Username'
               className='w-full input input-bordered h-10'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -35,6 +49,8 @@ const Login = () => {
               type='text'
               placeholder='Enter Password'
               className='w-full input input-bordered h-10'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
@@ -49,8 +65,15 @@ const Login = () => {
           </p>
 
           <div>
-            <button className='btn btn-block btn-sm mt-2 text-slate-800 bg-amber-600 hover:bg-amber-500 transition-colors ease-in-out delay-50'>
-              Login
+            <button
+              className='btn btn-block btn-sm mt-2 text-slate-800 bg-amber-600 hover:bg-amber-500 transition-colors ease-in-out delay-50'
+              disabled={loading}
+            >
+              {loading ? (
+                <span className='loading loading-spinner'></span>
+              ) : (
+                'Login'
+              )}
             </button>
           </div>
         </form>
